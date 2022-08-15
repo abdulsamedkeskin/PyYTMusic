d975d07fb0d69e8c7f99a3741e7750a8504c67e3
Abdulsamet Keskin <sametkeskin.py@gmail.com>
2022-08-15 17:16:32 +0300

    Update search.py

--- PyYTMusic/search.py
+++ PyYTMusic/search.py
@@ -10,12 +10,13 @@ ydl_opts = {
 
 
 class Search:
-    def search(self, query: str, filter: List) -> List[Dict]:
+    def search(self, query: str, filter: List = None) -> List[Dict]:
         filters = ['songs', "albums", "artists", "playlists",
                    "videos", "community playlists"]
-        for i in filter:
-            if i and i not in filters:
-                raise Exception("Invalid filter")
+        if filter is not None:
+            for i in filter:
+                if i and i not in filters:
+                    raise Exception("Invalid filter")
         UNAUTHORIZED_PAYLOAD.update({"query": query})
         req = requests.post(
             API_URL, json=UNAUTHORIZED_PAYLOAD)
@@ -25,18 +26,28 @@ class Search:
         for i in range(len(results)):
             try:
                 filter_query = (eval(FILTER)).lower()
-                if filter_query in filter or filter_query == "top result":
-                    for _ in range(len(eval(CONTENTS))):
-                        if filter_query == "videos" or filter_query == "songs" or filter_query == "top result":
-                            video_id = eval(VIDEO_ID)
-                            with YoutubeDL(ydl_opts) as ydl:
-                                player_url = ydl.extract_info(
-                                    f"https://music.youtube.com/watch?v={video_id}", download=False)
-                        data = {
-                            "type": filter_query,
-                            "name": eval(MUSIC_NAME),
-                            "thumbnails": eval(THUMBNAIL),
+                for _ in range(len(eval(CONTENTS))):
+                    if filter_query == "videos" or filter_query == "songs" or filter_query == "top result":
+                        video_id = eval(VIDEO_ID)
+                        with YoutubeDL(ydl_opts) as ydl:
+                            player_url = ydl.extract_info(
+                                f"https://music.youtube.com/watch?v={video_id}", download=False)
+                    data = {
+                        "type": filter_query,
+                        "name": eval(MUSIC_NAME),
+                        "thumbnails": eval(THUMBNAIL),
+                    }
+                    if filter is None:
+                        top_result_data = {
+                            "author": eval(AUTHOR),
+                            "videoId": video_id,
+                            "playerUrl": eval(MUSIC_URL),
+                            "duration": eval(DURATION),
                         }
+                        data = {**data, **top_result_data}
+                        search_results.append(data)
+                        raise StopIteration
+                    if filter_query in filter:
                         if filter_query == "top result":
                             top_result_data = {
                                 "author": eval(AUTHOR),
@@ -82,10 +93,11 @@ class Search:
                                 "song_count": eval(PLAYLIST_SONG_COUNT),
                             }
                             data = {**data, **playlist_data}
-
                         search_results.append(data)
             except KeyError:
                 pass
+            except StopIteration:
+                return {"results": search_results}
         return {
             "results": search_results,
         }
